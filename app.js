1599a0568187cc886383b091a0894ad02546fd29
juwonkang <107605848+juwonkang@users.noreply.github.com>
2023-09-03 22:23:23 +0900

    fuction 으로 데이터보내기와 argument 사용법

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-const a  = 5;   //const 는 바뀔수없다
-const b = 8;
-let minTonJuwon = "sexy";  //let은 다시설정가능
+function mother(NameOfPerson,age){
+    console.log("Hellow My name is" + NameOfPerson + "and I am " +  age + "years old ")
+    
+}
+mother( "juwon", 10)
+mother("pia", 20)
+mother("lotte", 30)
+console.log("asdlkfj")
 
-//var 도 있는데 이건 엄청 옛날거다 (업데이트가능)
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log( "You are " + minTonJuwon);
-
-minTonJuwon = "cool"; //let 업데이트
-
-console.log("You are " + minTonJuwon);
