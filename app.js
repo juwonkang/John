75338d742c4c0d30cf7383878ba1514e737bfa48
juwonkang <107605848+juwonkang@users.noreply.github.com>
2023-09-08 15:14:54 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
-const player = {
-    name:"juwon",
-    sayhellow : function(otherpersonname){
-        console.log("Hellow" + otherpersonname + "nice to meet you")
+const h1 = document.querySelector(".hello h1");
 
-    },
-    age : 20
+
+function handletitleclick(){
+    h1.style.color = "blue"
+    
 }
 
-console.log(player.name)
-player.sayhellow("kevin")
-player.name
+function handlemouseenter(){
+   h1.innerText ="mouse is here"
+}
+
+function handlemouseleave(){
+    h1.innerText = "mouse is gone"
+}
+
+function handlewindowresize(){
+    document.body.style.backgroundColor = "red";
+}
+
+function handlewindowcopy(){
+    alert("복사했다!!!!!") 
+}
+
+function handlewindowpaste(){
+    alert("붙여넣었다!!!")
+}
+
+function handlewindowoffline(){
+    alert("살려줘")
+}
+
+function handlewindowonline(){
+    alert("좋아쒀~")
+}
+    
+
+h1.addEventListener("click", handletitleclick);
+h1.addEventListener("mouseenter", handlemouseenter);
+h1.addEventListener("mouseleave", handlemouseleave);
+
+window.addEventListener("resize",handlewindowresize);
+window.addEventListener("copy", handlewindowcopy);
+window.addEventListener("paste", handlewindowpaste);
+window.addEventListener("offline",handlewindowoffline);
+window.addEventListener("online",handlewindowonline);
